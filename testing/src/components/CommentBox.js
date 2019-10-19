c4c4f4cbf02ef57040dea36dff9e06763838523f
yisus82 <djalma8@gmail.com>
2019-10-19 06:41:10 +0200

    CommentBox component

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,5 +1,36 @@
 import React from 'react';
 
-const CommentBox = () => <div>Comment Box</div>;
+const CommentBox = () => {
+  const [comment, setComment] = React.useState('');
+
+  /**
+   * Handles the change event
+   * @param {object} event Event object
+   */
+  const handleChange = event => setComment(event.target.value);
+
+  /**
+   * Handles the submit event
+   * @param {object} event Event object
+   */
+  const handleSubmit = event => {
+    event.preventDefault();
+    setComment('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>Add a comment</h4>
+      <textarea
+        style={{ resize: 'none' }}
+        value={comment}
+        onChange={handleChange}
+      />
+      <div>
+        <button type="submit">Submit comment</button>
+      </div>
+    </form>
+  );
+};
 
 export default CommentBox;
