0f16f9a28b457f09d7d31b44e7e80214e657138b
yisus82 <djalma8@gmail.com>
2019-10-19 06:26:23 +0200

    Refactor Tests for App component

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -3,14 +3,16 @@ import '../../setupTests';
 import { shallow } from 'enzyme';
 import App from '../App';
 
-it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
+let wrapped;
 
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+it('shows a comment box', () => {
   expect(wrapped).toContainExactlyOneMatchingElement('CommentBox');
 });
 
 it('shows a comment list', () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped).toContainExactlyOneMatchingElement('CommentList');
 });
