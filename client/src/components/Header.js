9ad732aa2456ac0404d0a46a8513fdf22ff89b30
yisus82 <djalma8@gmail.com>
2019-12-25 06:04:04 +0100

    Header styles

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const Header = () => (
-  <div>
-    <Link to="/">Redux Auth</Link>
-    <Link to="/signin">Sign in</Link>
-    <Link to="/signup">Sign up</Link>
-    <Link to="/signout">Sign out</Link>
-    <Link to="/feature">Feature</Link>
-  </div>
-);
+const Header = () => {
+  const token = useSelector(state => state.auth.token);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Link to="/">Redux Auth</Link>
+      {!token && (
+        <div>
+          <Link style={{ marginRight: '1rem' }} to="/signin">
+            Sign in
+          </Link>
+          <Link to="/signup">Sign up</Link>
+        </div>
+      )}
+      {token && (
+        <div>
+          <Link style={{ marginRight: '1rem' }} to="/feature">
+            Feature
+          </Link>
+          <Link to="/signout">Sign out</Link>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Header;
